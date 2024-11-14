9a34d1acb98ac73fd6bd484e8f1f47d74d8507bc
Kieran David Evans <keyz182@gmail.com>
2024-11-14 15:29:40 +0000

    Cookie cutter initial commit

---
+++ 1.5/Source/Mr_Samuel_Streamer_Flavour_Pack/Mr_Samuel_Streamer_Flavour_PackDefOf.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Mr_Samuel_Streamer_Flavour_Pack;
+
+[DefOf]
+public static class Mr_Samuel_Streamer_Flavour_PackDefOf
+{
+    // Remember to annotate any Defs that require a DLC as needed e.g.
+    // [MayRequireBiotech]
+    // public static GeneDef YourPrefix_YourGeneDefName;
+    
+    static Mr_Samuel_Streamer_Flavour_PackDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(Mr_Samuel_Streamer_Flavour_PackDefOf));
+}
