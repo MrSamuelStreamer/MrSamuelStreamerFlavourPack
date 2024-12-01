314c8ea1f35535d51fc4171c7d8818663262e6c7
Kieran David Evans <keyz182@gmail.com>
2024-12-01 15:19:35 +0000

    Signal Tracker

---
+++ 1.5/Source/MSSFP/Achievements/SignalTrackerTracker.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using AchievementsExpanded;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace MSSFP.Achievements;
+
+public class SignalTracker: TrackerBase, ISignalReceiver
+{
+    public string Signal;
+
+    public SignalTracker()
+    {
+        Find.SignalManager.RegisterReceiver(this);
+    }
+
+
+    public SignalTracker(SweetBabyBoyTracker reference) : base(reference)
+    {
+        Find.SignalManager.RegisterReceiver(this);
+    }
+
+    public override void ExposeData()
+    {
+        base.ExposeData();
+        Scribe_Values.Look(ref Signal, "Signal");
+    }
+
+    public override string Key
+    {
+        get { return "MSS_SignalTracker"; }
+        set { }
+    }
+
+    protected override string[] DebugText => [$"SignalTracker: {Signal}]"];
+
+    public override bool Trigger()
+    {
+        base.Trigger();
+
+        return true;
+    }
+
+    public void Notify_SignalReceived(Signal signal)
+    {
+        if (signal.tag != Signal)
+        {
+            return;
+        }
+
+        if (Current.ProgramState != ProgramState.Playing)
+        {
+            return;
+        }
+
+        AchievementCard card = AchievementPointManager.GetCards<SignalTracker>().FirstOrDefault(card => card.tracker == this);
+
+        if (card != null && Trigger())
+        {
+            card.UnlockCard();
+        }
+    }
+}
