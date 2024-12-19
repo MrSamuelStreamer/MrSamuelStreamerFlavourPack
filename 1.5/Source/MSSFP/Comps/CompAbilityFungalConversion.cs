a819c5e7b4b86188e4763a1d34f1b64e55bb9f5c
Kieran David Evans <keyz182@gmail.com>
2024-12-19 20:56:58 +0000

    Initial work

---
+++ 1.5/Source/MSSFP/Comps/CompAbilityFungalConversion.cs
@@ -0,0 +1,37 @@
+﻿using RimWorld;
+using Verse;
+
+namespace MSSFP.Comps;
+
+public class CompAbilityFungalConversion : CompAbilityEffect
+{
+
+        private System.Random rand = new System.Random();
+
+
+
+		public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+		{
+
+            base.Apply(target, dest);
+            FleckMaker.Static(target.Cell, this.parent.pawn.Map, MSSFPDefOf.PsycastPsychicEffect);
+
+            foreach (Thing current in target.Cell.GetThingList(this.parent.pawn.Map))
+            {
+                if (current is not Plant plantTarget) continue;
+
+                    PlantProperties plant = plantTarget.def.plant;
+                    bool flag = (plant != null);
+                    if (flag)
+                    {
+                        if (plant.IsTree && plantTarget.def.defName == "Plant_TreeGauranlen")
+                        {
+                            Plant thing2 = (Plant)GenSpawn.Spawn(MSSFPDefOf.MSSFP_Plant_TreeFroganlen, plantTarget.Position, plantTarget.Map, WipeMode.Vanish);
+                            Plant thingToDestroy = (Plant)plantTarget;
+                            thing2.Growth = thingToDestroy.Growth;
+                            plantTarget.Destroy();
+                        }
+                    }
+            }
+        }
+}
---
+++ 1.5/Source/MSSFP/Comps/CompProperties_AbilityFungalConversion.cs
@@ -0,0 +1,12 @@
+﻿using RimWorld;
+
+namespace MSSFP.Comps;
+
+public class CompProperties_AbilityFungalConversion: CompProperties_AbilityEffect
+{
+    public CompProperties_AbilityFungalConversion()
+    {
+        compClass = typeof(CompAbilityFungalConversion);
+    }
+
+}
