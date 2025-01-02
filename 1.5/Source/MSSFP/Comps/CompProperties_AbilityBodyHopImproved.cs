9eea940b3dfe3f969215f169fbb193898705f1d2
Kieran David Evans <keyz182@gmail.com>
2025-01-02 14:45:02 +0000

    Better possession

---
+++ 1.5/Source/MSSFP/Comps/CompProperties_AbilityBodyHopImproved.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+using Verse;
+
+namespace MSSFP.Comps;
+
+public class CompProperties_AbilityBodyHopImproved : CompProperties_AbilityEffect
+{
+    public HediffDef hediffOnSelf;
+    public CompProperties_AbilityBodyHopImproved()
+    {
+        compClass = typeof(CompAbilityEffect_BodyHopImproved);
+    }
+}
