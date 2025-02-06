530bad9bdb5d300477dfaee7bfe019c795d90ab4
Kieran David Evans <keyz182@gmail.com>
2025-02-06 22:25:02 +0000

    Squelch

---
+++ 1.5/Source/MSSFP-VFE/MSSFP-VFEMod.cs
@@ -0,0 +1,19 @@
+﻿using Verse;
+using HarmonyLib;
+
+namespace MSSFP.VFE;
+
+public class MSSFPVFEMod : Mod
+{
+
+    public MSSFPVFEMod(ModContentPack content) : base(content)
+    {
+        ModLog.Debug("Hello world from MSSFPVFEMod");
+
+#if DEBUG
+        Harmony.DEBUG = true;
+#endif
+        Harmony harmony = new Harmony("MrSamuelStreamer.rimworld.MSSFP.VFE.main");
+        harmony.PatchAll();
+    }
+}
