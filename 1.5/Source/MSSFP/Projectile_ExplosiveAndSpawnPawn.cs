0a6f3b67ecfcd6fb21f33307c2c10c23f692b1d9
Kieran David Evans <keyz182@gmail.com>
2025-02-13 18:26:55 +0000

    The canon cannon that shoots babies with scaria

---
+++ 1.5/Source/MSSFP/Projectile_ExplosiveAndSpawnPawn.cs
@@ -0,0 +1,29 @@
+﻿using Verse;
+
+namespace MSSFP;
+
+public class Projectile_ExplosiveAndSpawnPawn: Projectile_Explosive
+{
+    public PawnKindDef kind => DefDatabase<PawnKindDef>.AllDefsListForReading.FirstOrDefault(d => d.defName == "MSSFP_BabyCritter");
+
+    protected override void Impact(Thing hitThing, bool blockedByShield = false)
+    {
+        Map map = Map;
+        base.Impact(hitThing, blockedByShield);
+
+        if(kind == null) return;
+
+        IntVec3 loc = Position;
+
+        foreach (IntVec3 c in GenAdjFast.AdjacentCells8Way(Position))
+        {
+            if (c.GetFirstBuilding(map) == null && c.Standable(map) && Rand.Chance(.75f))
+            {
+                Pawn p = PawnGenerator.GeneratePawn(kind, Find.FactionManager.OfInsects);
+
+                GenSpawn.Spawn(p, loc, map);
+            }
+        }
+
+    }
+}
