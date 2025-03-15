ca10e18af16046841ddee6e30d9856c9b8bdc1fd
Kieran David Evans <keyz182@gmail.com>
2025-03-15 11:18:33 +0000

    Haunt fixes and optimisations

--- 1.5/Source/MSSFP/Comps/Map/HauntedMapComponent.cs
+++ 1.5/Source/MSSFP/Comps/Map/HauntedMapComponent.cs
@@ -9,45 +9,52 @@ namespace MSSFP.Comps.Map;
 public class HauntedMapComponent(Verse.Map map) : MapComponent(map)
 {
     public int LastFiredTick = 0;
+    public int SearchRadius = 10;
+
+    public IEnumerable<Building_Grave> Graves => map.listerThings.AllThings.OfType<Building_Grave>()
+        .Where(g => g.Corpse != null);
+
+    public IEnumerable<Pawn> PawnsNearGraves => Graves
+        .SelectMany(thing => GenRadial.RadialCellsAround(thing.Position, SearchRadius, true))
+        .Distinct()
+        .SelectMany(cell =>
+            map.thingGrid
+                .ThingsAt(cell))
+        .OfType<Pawn>();
+
+    public IEnumerable<Pawn> PawnPool => PawnsNearGraves.Where(pawn=>
+            !pawn.health.hediffSet.HasHediff(MSSFPDefOf.MSS_FP_PawnDisplayer)
+            &&
+            !pawn.health.hediffSet.HasHediff(MSSFPDefOf.MSS_FP_FroggeHaunt));
 
     public override void MapComponentTick()
     {
         if(!MSSFPMod.settings.ShowHaunts) return;
 
         //It's ok if we miss some ticks, so the simple check is fine
-        if (LastFiredTick + 2 * GenDate.TicksPerDay < Find.TickManager.TicksGame)
+        bool skip = false;
+        if (!skip && LastFiredTick + 2 * GenDate.TicksPerDay >= Find.TickManager.TicksGame) return;
+
+        LastFiredTick = Find.TickManager.TicksGame + 2 * GenDate.TicksPerHour;
+
+        Pawn pawn = PawnPool.RandomElementWithFallback();
+        if(pawn == null) return;
+
+        Building_Grave grave = GenRadial.RadialCellsAround(pawn.Position, SearchRadius, true).SelectMany(cell=>Graves.Where(g=>g.Position == cell)).RandomElementWithFallback();
+
+        LastFiredTick += 4 * GenDate.TicksPerDay;
+
+        if (pawn.def == MSSFPDefOf.MSSFP_Frogge)
         {
-            LastFiredTick = Find.TickManager.TicksGame + 2 * GenDate.TicksPerHour;
+            pawn.health.AddHediff(MSSFPDefOf.MSS_FP_FroggeHaunt);
+        }
+        else
+        {
+            Hediff hediff = pawn.health.AddHediff(MSSFPDefOf.MSS_FP_PawnDisplayer);
 
-            IEnumerable<Thing> graves = map.listerThings.ThingsOfDef(ThingDefOf.Grave)
-                .Where(grave => grave is Building_Grave { Corpse: not null } or Building_Sarcophagus { Corpse: not null });
-
-            IEnumerable<Pawn> pawnPool = graves
-                .SelectMany(thing=>GenRadial.RadialCellsAround(thing.Position, 10, false))
-                .Distinct()
-                .SelectMany(cell=>
-                    map.thingGrid
-                        .ThingsAt(cell))
-                .OfType<Pawn>()
-                .Where(pawn=>!pawn.health.hediffSet.HasHediff(MSSFPDefOf.MSS_FP_PawnDisplayer) && !pawn.health.hediffSet.HasHediff(MSSFPDefOf.MSS_FP_FroggeHaunt));
-
-            Pawn pawn = pawnPool.RandomElementWithFallback();
-
-            if(pawn == null) return;
-            LastFiredTick += 4*GenDate.TicksPerDay;
-
-            if (pawn.def == MSSFPDefOf.MSSFP_Frogge)
+            if (hediff.TryGetComp(out HediffComp_Haunt comp))
             {
-                pawn.health.AddHediff(MSSFPDefOf.MSS_FP_FroggeHaunt);
-            }
-            else
-            {
-                Hediff hediff = pawn.health.AddHediff(MSSFPDefOf.MSS_FP_PawnDisplayer);
-
-                // if (hediff.TryGetComp(out HediffComp_Haunt comp))
-                // {
-                //     comp.SetPawnToDraw(pawn);
-                // }
+                comp.SetPawnToDraw(grave.Corpse.InnerPawn);
             }
         }
     }
