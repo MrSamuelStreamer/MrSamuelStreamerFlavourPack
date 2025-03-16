63aef644fe47533bb731147c8387ff4066450d47
Kieran David Evans <keyz182@gmail.com>
2025-03-16 13:44:36 +0000

    Witch related incidents, and some task scheduling

---
+++ 1.5/Source/MSSFP/Hediffs/HiddenHediffWithComps.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using Verse;
+
+namespace MSSFP.Hediffs;
+
+public class HiddenHediffWithComps : HediffWithComps
+{
+    public static Lazy<FieldInfo> VisibleInfo = new(() => AccessTools.Field(typeof(HediffWithComps), "visible"));
+
+    public override void PostMake()
+    {
+        VisibleInfo.Value.SetValue(this, false);
+        base.PostMake();
+    }
+}
