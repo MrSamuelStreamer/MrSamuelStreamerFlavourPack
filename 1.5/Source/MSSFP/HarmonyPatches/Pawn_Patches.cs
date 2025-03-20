6fb3144a99791f5504af0eb4762f01e6f2787d62
Kieran David Evans <keyz182@gmail.com>
2025-03-20 16:35:10 +0000

    Disable test haunt

--- 1.5/Source/MSSFP/HarmonyPatches/Pawn_Patches.cs
+++ 1.5/Source/MSSFP/HarmonyPatches/Pawn_Patches.cs
@@ -9,29 +9,29 @@ namespace MSSFP.HarmonyPatches;
 [HarmonyPatch(typeof(Pawn))]
 public static class Pawn_Patches
 {
-    [HarmonyPatch(nameof(Pawn.GetGizmos))]
-    [HarmonyPostfix]
-    public static void Pawn_GetGizmos_Postfix(Pawn __instance, ref IEnumerable<Gizmo> __result)
-    {
-        List<Gizmo> gizmos = new(__result);
-        if (__instance.RaceProps.Humanlike)
-        {
-            gizmos.Add(
-                new Command_Action
-                {
-                    defaultLabel = "MSSFP_TESTHAUNT".Translate(),
-                    defaultDesc = "MSSFP_TESTHAUNTDESC".Translate(),
-                    // icon = ContentFinder<Texture2D>.Get("UI/MSS_FP_Haunts_Toggle"),
-                    action = delegate
-                    {
-                        __instance.Map.GetComponent<HauntAnimationController>().StartHaunting(__instance);
-                    },
-                }
-            );
-        }
-
-        __result = gizmos;
-    }
+    // [HarmonyPatch(nameof(Pawn.GetGizmos))]
+    // [HarmonyPostfix]
+    // public static void Pawn_GetGizmos_Postfix(Pawn __instance, ref IEnumerable<Gizmo> __result)
+    // {
+    //     List<Gizmo> gizmos = new(__result);
+    //     if (__instance.RaceProps.Humanlike)
+    //     {
+    //         gizmos.Add(
+    //             new Command_Action
+    //             {
+    //                 defaultLabel = "MSSFP_TESTHAUNT".Translate(),
+    //                 defaultDesc = "MSSFP_TESTHAUNTDESC".Translate(),
+    //                 // icon = ContentFinder<Texture2D>.Get("UI/MSS_FP_Haunts_Toggle"),
+    //                 action = delegate
+    //                 {
+    //                     __instance.Map.GetComponent<HauntAnimationController>().StartHaunting(__instance);
+    //                 },
+    //             }
+    //         );
+    //     }
+    //
+    //     __result = gizmos;
+    // }
 
     // /// <summary>
     // /// Draw haunts
