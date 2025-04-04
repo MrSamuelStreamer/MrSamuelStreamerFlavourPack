51fbf605c9d3d5226fa3c2017bb0c90cba3eea4c
Kieran David Evans <keyz182@gmail.com>
2025-04-04 12:54:31 +0100

    Ritual tweaks

--- 1.5/Source/MSSFP-VFE/RitualPosition_Sit.cs
+++ 1.5/Source/MSSFP-VFE/RitualPosition_Sit.cs
@@ -1,7 +1,9 @@
-﻿using RimWorld;
+﻿using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
 using Verse;
 
-namespace MSSFP.Rituals;
+namespace MSSFP.VFE;
 
 public class RitualPosition_Sit : RitualPosition
 {
@@ -10,10 +12,21 @@ public class RitualPosition_Sit : RitualPosition
 
     public override PawnStagePosition GetCell(IntVec3 spot, Pawn p, LordJob_Ritual ritual)
     {
-        Pawn pawn = ofPawn == null ? p : ritual.assignments.FirstAssignedPawn(ofPawn);
+        // Pawn pawn = ofPawn == null ? p : ritual.assignments.FirstAssignedPawn(ofPawn);
 
         if (ritual.selectedTarget != null && ritual.selectedTarget.Thing.def.defName == "MSSFP_CuckChair")
             return new PawnStagePosition(ritual.selectedTarget.Thing.Position, null, Rot4.Invalid, highlight);
+
+        List<Building_CuckChair> chairs = GenRadial
+            .RadialCellsAround(ritual.selectedTarget.Thing.Position, 20, true)
+            .SelectMany(pos => ritual.selectedTarget.Thing.Map.thingGrid.ThingsAt(pos))
+            .OfType<Building_CuckChair>()
+            .ToList();
+
+        if (chairs.Count > 0)
+        {
+            return new PawnStagePosition(chairs.RandomElement().Position, null, Rot4.Invalid, highlight);
+        }
         return new PawnStagePosition(IntVec3.Invalid, null, Rot4.Invalid, highlight);
     }
 
