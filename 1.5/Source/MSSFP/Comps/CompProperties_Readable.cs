89da5bd79f8afa58daccd57b1c70e091308f897d
Kieran David Evans <keyz182@gmail.com>
2025-04-12 15:19:00 +0100

    Readable comp

---
+++ 1.5/Source/MSSFP/Comps/CompProperties_Readable.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using Verse;
+
+namespace MSSFP.Comps;
+
+public class CompProperties_Readable : CompProperties
+{
+    public LetterDef letterDef;
+    public string letterLabel;
+    public string letterText;
+    public string buttonLabel = "Read";
+    public string buttonDesc = "Read the letter";
+    public string buttonIcon;
+
+    public bool destroyOnRead = false;
+    public Texture2D buttonIconTex => buttonIcon is not null ? ContentFinder<Texture2D>.Get(buttonIcon) : BaseContent.BadTex;
+
+    public CompProperties_Readable()
+    {
+        compClass = typeof(CompReadable);
+    }
+}
---
+++ 1.5/Source/MSSFP/Comps/CompReadable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace MSSFP.Comps;
+
+public class CompReadable : ThingComp
+{
+    public CompProperties_Readable Props => (CompProperties_Readable)props;
+
+    public override IEnumerable<Gizmo> CompGetGizmosExtra()
+    {
+        foreach (Gizmo gizmo in base.CompGetGizmosExtra())
+        {
+            yield return gizmo;
+        }
+
+        Command_Action read = new()
+        {
+            defaultLabel = Props.buttonLabel,
+            defaultDesc = Props.buttonDesc,
+            icon = Props.buttonIconTex,
+            action = Read,
+        };
+
+        yield return read;
+    }
+
+    public virtual void Read()
+    {
+        Find.LetterStack.ReceiveLetter(Props.letterLabel.Formatted(), Props.letterText.Formatted(), Props.letterDef);
+        parent.Destroy();
+    }
+
+    public override IEnumerable<FloatMenuOption> CompFloatMenuOptions(Pawn selPawn)
+    {
+        foreach (FloatMenuOption compFloatMenuOption in base.CompFloatMenuOptions(selPawn))
+        {
+            yield return compFloatMenuOption;
+        }
+
+        yield return new FloatMenuOption(Props.buttonLabel, Read, MenuOptionPriority.High);
+    }
+}
