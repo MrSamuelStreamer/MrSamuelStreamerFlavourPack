e41abff35affa93d57fa95626ca9c7c1655ca8a0
Kieran David Evans <keyz182@gmail.com>
2025-05-27 13:00:36 +0100

    CompProperties_AbilityUnfamiliar

---
+++ 1.5/Source/MSSFP/Psycasts/CompProperties_AbilityUnfamiliar.cs
@@ -0,0 +1,11 @@
+﻿using RimWorld;
+
+namespace MSSFP.Psycasts;
+
+public class CompProperties_AbilityUnfamiliar : CompProperties_AbilityEffect
+{
+    public CompProperties_AbilityUnfamiliar()
+    {
+        compClass = typeof(CompAbilityUnfamiliar);
+    }
+}
