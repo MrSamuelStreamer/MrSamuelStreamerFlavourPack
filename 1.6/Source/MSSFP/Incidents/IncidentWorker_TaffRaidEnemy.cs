b457bda3bca51d3be6603ba9da27fb7463fe7517
Kieran David Evans <keyz182@gmail.com>
2025-06-25 10:40:58 +0100

    Taff raid fix

--- 1.6/Source/MSSFP/Incidents/IncidentWorker_TaffRaidEnemy.cs
+++ 1.6/Source/MSSFP/Incidents/IncidentWorker_TaffRaidEnemy.cs
@@ -8,11 +8,9 @@ public class IncidentWorker_TaffRaidEnemy : IncidentWorker_RaidEnemy
     protected override bool TryExecuteWorker(IncidentParms parms)
     {
         parms.faction = Find.FactionManager.FirstFactionOfDef(MSSFPDefOf.MSSFP_TaffsFaction);
-        if (parms.faction == null)
-        {
-            FactionGenerator.CreateFactionAndAddToManager(MSSFPDefOf.MSSFP_TaffsFaction);
-            parms.faction = Find.FactionManager.FirstFactionOfDef(MSSFPDefOf.MSSFP_TaffsFaction);
-        }
+
+        if (parms.faction == null || parms.faction.defeated || parms.faction.AllyOrNeutralTo(Faction.OfPlayer))
+            return false;
 
         return base.TryExecuteWorker(parms);
     }
