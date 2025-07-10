8a23efaf66752b0e189a10bcc7020e5c43d62623
Kieran David Evans <keyz182@gmail.com>
2025-07-10 20:53:21 +0100

    Fix laggy patch

--- 1.5/Source/MSSFP/HarmonyPatches/SkillRecord_Patch.cs
+++ 1.5/Source/MSSFP/HarmonyPatches/SkillRecord_Patch.cs
@@ -12,15 +12,19 @@ namespace MSSFP.HarmonyPatches;
 [HarmonyPatch(typeof(SkillRecord))]
 public static class SkillRecord_Patch
 {
-    [HarmonyPatch(nameof(SkillRecord.Aptitude), MethodType.Getter)]
-    [HarmonyPostfix]
-    public static void AptitudeFor(SkillRecord __instance, ref int __result)
-    {
-        __result += __instance
-            .Pawn.health.hediffSet.hediffs.OfType<HediffWithComps>()
-            .SelectMany(hediff => hediff.comps)
-            .OfType<HediffComp_Haunt>()
-            .Where(comp => comp.skillToBoost == __instance.def)
-            .Sum(comp => comp.SkillBoostLevel);
-    }
+    // [HarmonyPatch(nameof(SkillRecord.Aptitude), MethodType.Getter)]
+    // [HarmonyPostfix]
+    // public static void AptitudeFor(SkillRecord __instance, ref int __result)
+    // {
+    //     return; // TODO: Fix it
+    //     if (!__instance.Pawn.health.hediffSet.hediffs.Any(h => h is HediffWithComps hwc && hwc.comps.Any(c => c is HediffComp_Haunt)))
+    //         return;
+    //
+    //     __result += __instance
+    //         .Pawn.health.hediffSet.hediffs.OfType<HediffWithComps>()
+    //         .SelectMany(hediff => hediff.comps)
+    //         .OfType<HediffComp_Haunt>()
+    //         .Where(comp => comp.skillToBoost == __instance.def)
+    //         .Sum(comp => comp.SkillBoostLevel);
+    // }
 }
