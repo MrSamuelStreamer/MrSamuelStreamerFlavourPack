cde00d1c7e6923cb9579a5670c3583dd36481ada
Dexter Lowe <feldoh10@gmail.com>
2025-09-20 05:58:20 +0100

    Pursuer throw off item and reset

--- 1.6/Source/MSSFP/Questing/ScenPart_Pursuers.cs
+++ 1.6/Source/MSSFP/Questing/ScenPart_Pursuers.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
+using LudeonTK;
 using RimWorld;
 using UnityEngine;
 using Verse;
@@ -44,8 +44,8 @@ public class PursuersModExtension : DefModExtension
     public int minRaidPoints = 5000;
     public float raidPointMultiplier = 1.5f;
     public int gravEngineCheckInterval = 2500;
-    public FactionDef faction = FactionDefOf.Mechanoid;
-    public PawnsArrivalModeDef raidArrivalMode = PawnsArrivalModeDefOf.RandomDrop;
+    public FactionDef faction;
+    public PawnsArrivalModeDef raidArrivalMode;
     public List<MapGeneratorDef> safeMapGenerators = [];
     public List<LandmarkDef> safeLandmarks = [];
     public string alertPursuerThreatCriticalText;
@@ -56,27 +56,48 @@ public class PursuersModExtension : DefModExtension
     public string letterTextPursuerThreat;
     public string letterLabelPursuerThreatFoiled;
     public string letterTextPursuerThreatFoiled;
+
+    public override void ResolveReferences(Def parentDef)
+    {
+        base.ResolveReferences(parentDef);
+        faction ??= FactionDefOf.Mechanoid;
+        raidArrivalMode ??= PawnsArrivalModeDefOf.RandomDrop;
+    }
 }
 
 public class ScenPart_Pursuers : ScenPart
 {
     public bool onStartMap = true;
-    private Dictionary<Map, int> mapWarningTimers = new();
-    private Dictionary<Map, int> mapRaidTimers = new();
-    private bool questCompleted;
-    private Map cachedAlertMap;
-    private Alert_PursuerThreat alertCached;
-    private bool hasGravEngineCached = true;
-    private int lastCheckedGravEngineTick = -999999;
-    private List<Map> tmpWarningKeys;
-    private List<int> tmpWarningValues;
-    private List<Map> tmpRaidKeys;
-    private List<int> tmpRaidValues;
-    private int startMapId = -1;
+    public Dictionary<Map, int> mapWarningTimers = new();
+    public Dictionary<Map, int> mapRaidTimers = new();
+    public bool questCompleted;
+    public Map cachedAlertMap;
+    public Alert_PursuerThreat alertCached;
+    public bool hasGravEngineCached = true;
+    public int lastCheckedGravEngineTick = -999999;
+    public List<Map> tmpWarningKeys;
+    public List<int> tmpWarningValues;
+    public List<Map> tmpRaidKeys;
+    public List<int> tmpRaidValues;
+    public int startMapId = -1;
 
-    private Lazy<PursuersModExtension> pursuersModExtension => new(() => def.GetModExtension<PursuersModExtension>());
+    [DebugAction("Scenario", "Reset Pursuer Timers", actionType = DebugActionType.Action, allowedGameStates = AllowedGameStates.PlayingOnMap)]
+    public static void ResetPursuerScenPart()
+    {
+        foreach (ScenPart_Pursuers scenPart in Find.Scenario.AllParts.OfType<ScenPart_Pursuers>())
+        {
+            scenPart.PostWorldGenerate();
+            foreach (Map map in Find.Maps)
+            {
+                scenPart.PostMapGenerate(map);
+            }
+        }
+        Log.Message("Pursuer timers reset.");
+    }
 
-    private Alert_PursuerThreat AlertCached
+    public Lazy<PursuersModExtension> pursuersModExtension => new(() => def.GetModExtension<PursuersModExtension>());
+
+    public Alert_PursuerThreat AlertCached
     {
         get
         {
@@ -99,7 +120,7 @@ public class ScenPart_Pursuers : ScenPart
         }
     }
 
-    private bool Disabled => questCompleted || (pursuersModExtension.Value.gravEngineCheckInterval > 0 && !hasGravEngineCached);
+    public virtual bool Disabled => questCompleted || (pursuersModExtension.Value.gravEngineCheckInterval > 0 && !hasGravEngineCached);
 
     public override bool OverrideDangerMusic => onStartMap;
 
@@ -115,6 +136,7 @@ public class ScenPart_Pursuers : ScenPart
                 mapRaidTimers.Remove(key);
             }
         }
+
         Scribe_Values.Look(ref onStartMap, "initialMap");
         Scribe_Values.Look(ref startMapId, "startMapId", -1);
         Scribe_Collections.Look(ref mapWarningTimers, "mapWarningTimers", LookMode.Reference, LookMode.Value, ref tmpWarningKeys, ref tmpWarningValues);
@@ -125,6 +147,7 @@ public class ScenPart_Pursuers : ScenPart
             mapWarningTimers ??= new Dictionary<Map, int>();
             mapRaidTimers ??= new Dictionary<Map, int>();
         }
+
         lastCheckedGravEngineTick = -999999;
     }
 
@@ -152,6 +175,16 @@ public class ScenPart_Pursuers : ScenPart
         onStartMap = false;
     }
 
+    public virtual void AdjustTimers(int ticks, Map map)
+    {
+        if (mapWarningTimers.ContainsKey(map))
+            mapWarningTimers[map] = Math.Max(Find.TickManager.TicksGame + 1, mapWarningTimers[map] + ticks);
+        if (mapRaidTimers.ContainsKey(map))
+            mapRaidTimers[map] = Math.Max(Find.TickManager.TicksGame + 1, mapRaidTimers[map] + ticks);
+        if (cachedAlertMap != map || AlertCached == null) return;
+        AlertCached.raidTick = mapRaidTimers[map];
+    }
+
     public override void Tick()
     {
         if (pursuersModExtension.Value.gravEngineCheckInterval > 0 && Find.TickManager.TicksGame > lastCheckedGravEngineTick + pursuersModExtension.Value.gravEngineCheckInterval)
@@ -159,6 +192,7 @@ public class ScenPart_Pursuers : ScenPart
             hasGravEngineCached = GravshipUtility.PlayerHasGravEngine();
             lastCheckedGravEngineTick = Find.TickManager.TicksGame;
         }
+
         if (Disabled)
         {
             foreach (Map map in Find.Maps)
@@ -176,9 +210,11 @@ public class ScenPart_Pursuers : ScenPart
             {
                 if (Find.TickManager.TicksGame == mapWarningTimers[key])
                 {
-                    Find.LetterStack.ReceiveLetter(pursuersModExtension.Value.letterLabelPursuerThreat, pursuersModExtension.Value.letterTextPursuerThreat, LetterDefOf.ThreatSmall);
+                    Find.LetterStack.ReceiveLetter(pursuersModExtension.Value.letterLabelPursuerThreat, pursuersModExtension.Value.letterTextPursuerThreat,
+                        LetterDefOf.ThreatSmall);
                 }
             }
+
             foreach (Map key in mapRaidTimers.Keys)
             {
                 if (Find.TickManager.TicksGame >= mapRaidTimers[key] && (Find.TickManager.TicksGame - mapRaidTimers[key]) % 30000 == 0)
@@ -187,7 +223,7 @@ public class ScenPart_Pursuers : ScenPart
         }
     }
 
-    private void StartTimers(Map map)
+    public virtual void StartTimers(Map map)
     {
         bool safe = pursuersModExtension.Value.safeMapGenerators.Contains(map.generatorDef);
         if (!safe && Find.World.landmarks.landmarks.TryGetValue(map.Tile, out Landmark landmark) && pursuersModExtension.Value.safeLandmarks.Contains(landmark.def))
@@ -197,7 +233,8 @@ public class ScenPart_Pursuers : ScenPart
 
         if (safe)
         {
-            Find.LetterStack.ReceiveLetter(pursuersModExtension.Value.letterLabelPursuerThreatFoiled, pursuersModExtension.Value.letterTextPursuerThreatFoiled, LetterDefOf.PositiveEvent);
+            Find.LetterStack.ReceiveLetter(pursuersModExtension.Value.letterLabelPursuerThreatFoiled, pursuersModExtension.Value.letterTextPursuerThreatFoiled,
+                LetterDefOf.PositiveEvent);
             return;
         }
 
@@ -215,7 +252,7 @@ public class ScenPart_Pursuers : ScenPart
 
     public void Notify_QuestCompleted() => questCompleted = true;
 
-    private void FireRaid(Map map)
+    public virtual void FireRaid(Map map)
     {
         IncidentDefOf.RaidEnemy.Worker.TryExecute(new IncidentParms
         {
@@ -230,9 +267,36 @@ public class ScenPart_Pursuers : ScenPart
 
     public override IEnumerable<Alert> GetAlerts()
     {
-        Map currentMap = Find.CurrentMap;
-        if (currentMap is { IsPlayerHome: true } && AlertCached != null)
+        if (Find.CurrentMap is { IsPlayerHome: true } && AlertCached != null)
             yield return AlertCached;
     }
 
+}
+
+public class CompProperties_UseEffectThrowOffPursuer : CompProperties_UseEffect
+{
+    public int adjustTicks = 60000;
+
+    public CompProperties_UseEffectThrowOffPursuer()
+    {
+        compClass = typeof(Comp_UseEffectThrowOffPursuer);
+    }
+}
+
+public class Comp_UseEffectThrowOffPursuer : CompUseEffect
+{
+    public CompProperties_UseEffectThrowOffPursuer Props => (CompProperties_UseEffectThrowOffPursuer) props;
+
+    public override void DoEffect(Pawn pawn)
+    {
+        if (parent.Spawned)
+        {
+            foreach (ScenPart_Pursuers scenPart in Find.Scenario.AllParts.OfType<ScenPart_Pursuers>())
+            {
+                if (scenPart.Disabled) continue;
+                scenPart.AdjustTimers(Props.adjustTicks, pawn.Map);
+            }
+        }
+        Messages.Message("MSSFP_ThrowOffPursuerUseMessage".Translate(), pawn, MessageTypeDefOf.PositiveEvent, false);
+    }
 }
