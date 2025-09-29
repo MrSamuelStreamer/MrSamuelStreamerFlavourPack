8f9c576215ab1d5355b56821b1a7a7b7af133df6
Dexter Lowe <feldoh10@gmail.com>
2025-09-29 10:24:22 +0100

    Disable timed raids if safe from pursuers

---
+++ 1.6/Source/MSSFP/HarmonyPatches/TimedDetectionRaids_CompTickInterval_Patch.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using MSSFP.Questing;
+using Verse;
+
+namespace MSSFP.HarmonyPatches;
+
+using HarmonyLib;
+using RimWorld.Planet;
+
+[HarmonyPatch(typeof(TimedDetectionRaids), nameof(TimedDetectionRaids.CompTickInterval))]
+public static class TimedDetectionRaids_CompTickInterval_Patch
+{
+    [HarmonyPrefix]
+    static bool Prefix(TimedDetectionRaids __instance)
+    {
+        if (__instance.parent is not MapParent { HasMap: true } mapParent) return true;
+        foreach (ScenPart_Pursuers scenPart in Find.Scenario.AllParts.OfType<ScenPart_Pursuers>())
+        {
+            if (scenPart.MapIsSafeNow(mapParent.Map))
+            {
+                // Skip original method if any active Pursuers scenario part considers the map safe
+                return false;
+            }
+        }
+        return true;
+    }
+}
