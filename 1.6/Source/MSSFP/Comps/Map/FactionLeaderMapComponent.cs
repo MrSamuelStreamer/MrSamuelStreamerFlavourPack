55fe9fa1c6333f09a4267bf9a65a4ef351416af7
Kieran David Evans <keyz182@gmail.com>
2025-11-23 09:36:48 +0000

    Fix null ref

--- 1.6/Source/MSSFP/Comps/Map/FactionLeaderMapComponent.cs
+++ 1.6/Source/MSSFP/Comps/Map/FactionLeaderMapComponent.cs
@@ -17,14 +17,26 @@ public class FactionLeaderMapComponent(Verse.Map map) : MapComponent(map)
     public override void MapComponentTick()
     {
         base.MapComponentTick();
-        if(!haveChecked)
+        if (haveChecked)
         {
-            if (map.mapPawns.AllPawns.Any(pawn => pawn.Faction == map.ParentFaction && map.ParentFaction.leader == pawn))
-            {
-                Letter letter = LetterMaker.MakeLetter("MSSFP_LeaderFoundLabel".Translate(), "MSSFP_LeaderFoundText".Translate(map.ParentFaction.leader.Named("LEADER")), map.ParentFaction.HostileTo(Faction.OfPlayer) ? LetterDefOf.ThreatBig: LetterDefOf.NeutralEvent);
-                letter.lookTargets = new LookTargets([map.ParentFaction.leader]);
-                Find.LetterStack.ReceiveLetter(letter);
-            }
+            return;
         }
+
+        if (map?.mapPawns == null || map.mapPawns.AllPawns.NullOrEmpty())
+        {
+            return;
+        }
+
+        Faction faction = map.ParentFaction ?? map.Parent.Faction;
+        if (faction == null) return;
+
+        if (!map.mapPawns.AllPawns.Any(pawn => pawn.Faction == faction && faction.leader == pawn))
+        {
+            return;
+        }
+
+        Letter letter = LetterMaker.MakeLetter("MSSFP_LeaderFoundLabel".Translate(), "MSSFP_LeaderFoundText".Translate(faction.leader.Named("LEADER")), faction.HostileTo(Faction.OfPlayer) ? LetterDefOf.ThreatBig: LetterDefOf.NeutralEvent);
+        letter.lookTargets = new LookTargets([faction.leader]);
+        Find.LetterStack.ReceiveLetter(letter);
     }
 }
