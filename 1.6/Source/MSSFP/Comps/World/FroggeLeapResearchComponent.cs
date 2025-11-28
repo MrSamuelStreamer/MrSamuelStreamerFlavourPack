2e796e4c60ab121e6d7cc75a1095e2b10a11eca4
Kieran David Evans <keyz182@gmail.com>
2025-11-28 09:04:08 +0000

    Some backward compatibility

---
+++ 1.6/Source/MSSFP/Comps/World/FroggeLeapResearchComponent.cs
@@ -0,0 +1,8 @@
+using RimWorld.Planet;
+
+namespace MSSFP.Comps.World;
+
+public class FroggeLeapResearchComponent(RimWorld.Planet.World world) : WorldComponent(world)
+{
+    // Dummy for backwards compatibility
+}
---
+++ 1.6/Source/MSSFP/Comps/World/SpiritOfFroggeMapComponent.cs
@@ -0,0 +1,8 @@
+using RimWorld.Planet;
+
+namespace MSSFP.Comps.World;
+
+public class SpiritOfFroggeMapComponent(RimWorld.Planet.World world) : WorldComponent(world)
+{
+    // Dummy for backwards compatibility
+}
