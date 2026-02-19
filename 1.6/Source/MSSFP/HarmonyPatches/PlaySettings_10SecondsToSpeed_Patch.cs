48c826fff1e095822c3967a42f44ec5f4e204145
RetroBoy <retroboy9928@gmail.com>
2026-02-19 23:00:36 +0200

    10 seocon widget now is gone when disable in settings

--- 1.6/Source/MSSFP/HarmonyPatches/PlaySettings_10SecondsToSpeed_Patch.cs
+++ 1.6/Source/MSSFP/HarmonyPatches/PlaySettings_10SecondsToSpeed_Patch.cs
@@ -10,12 +10,12 @@ internal static class PlaySettings_10SecondsToSpeed_Patch
 {
     [HarmonyPostfix]
     private static void Postfix(WidgetRow row, bool worldView)
-    {
-        if (worldView)
+     {
+        if (worldView || !MSSFPMod.settings.Enable10SecondsToSpeed)
             return;
 
         row.ToggleableIcon(
-            ref MSSFPMod.settings.Enable10SecondsToSpeed,
+            ref MSSFPMod.settings.Active10SecondsToSpeed,
             ContentFinder<Texture2D>.Get("UI/MSS_FP_10Seconds", true),
             "MSSFP_10SecondsToSpeed_Tooltip".Translate(),
             SoundDefOf.Mouseover_ButtonToggle,
