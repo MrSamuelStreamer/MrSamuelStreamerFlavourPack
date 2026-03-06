0917c130761aebbde3be46d08b0f27127c88980a
Kieran David Evans <keyz182@gmail.com>
2026-03-06 22:47:03 +0000

    Elevation Overlay

---
+++ 1.6/Source/MSSFP/HarmonyPatches/PlaySettings_ElevationOverlay_Patch.cs
@@ -0,0 +1,27 @@
+using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace MSSFP.HarmonyPatches;
+
+[HarmonyPatch(typeof(PlaySettings), nameof(PlaySettings.DoPlaySettingsGlobalControls))]
+internal static class PlaySettings_ElevationOverlay_Patch
+{
+    private static readonly Texture2D ElevationTex = ContentFinder<Texture2D>.Get("UI/Overlays/Elevation", true);
+
+    [HarmonyPostfix]
+    private static void Postfix(WidgetRow row, bool worldView)
+    {
+        if (!worldView)
+            return;
+
+        row.ToggleableIcon(
+            ref MSSFPMod.settings.ShowElevationOverlay,
+            ElevationTex,
+            "MSSFP_ElevationOverlay_Tooltip".Translate(),
+            SoundDefOf.Mouseover_ButtonToggle,
+            null
+        );
+    }
+}
