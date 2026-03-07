849185d2dcd78dbcddd627d7c84c4295c63b5bb2
Kieran David Evans <keyz182@gmail.com>
2026-03-07 16:34:39 +0000

    Clear all landmarks

---
+++ 1.6/Source/MSSFP/DebugActions/DebugActions_ClearLandmarks.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using LudeonTK;
+using RimWorld.Planet;
+using Verse;
+using MSSFP.Comps.World;
+using RimWorld;
+using UnityEngine;
+
+namespace MSSFP.DebugActions;
+
+public static class DebugActions_ClearLandmarks
+{
+    private const string Category = "Landmarks";
+
+    [DebugAction(Category, "Clear All Landmarks", allowedGameStates = AllowedGameStates.PlayingOnWorld)]
+    public static void ClearLandmarks()
+    {
+        foreach (Tile tile in Find.WorldGrid.Surface.Tiles)
+        {
+            Find.World.landmarks[tile.tile] = null;
+        }
+    }
+
+}
