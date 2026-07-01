6d39db2d21cc3f6960f1ef1d70e8b79efaffcda2
chris-b-90 <kedjown@gmail.com>
2026-07-01 01:14:38 +0100

    lower storyteller ageing rates slider options

---
+++ 1.6/Source/MSSFP/HarmonyPatches/Ageing_StorytellerPatch.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+
+namespace MSSFP.HarmonyPatches;
+
+/// <summary>
+/// Prefix to allow the ageing slider in difficulty settings to go to 0.
+/// Additionally to allow decimals to one point in the slider, if necessary.
+/// </summary>
+
+[HarmonyPatch]
+public static class AgeingStorytellerPatch
+{
+    public static MethodBase TargetMethod()
+    {
+        return AccessTools.GetDeclaredMethods(typeof(StorytellerUI))
+            .First(method =>
+                method.Name == "DrawCustomDifficultySlider"
+                && method.GetParameters().Any(p => p.Name == "optionName")
+                && method.GetParameters().Any(p => p.Name == "min")
+                && method.GetParameters().Any(p => p.Name == "precision"));
+    }
+    [HarmonyPrefix]
+    public static void Prefix(
+        string optionName,
+        ref float min,
+        ref float precision)
+    {
+        if (optionName != "childAgingRate" && optionName != "adultAgingRate")
+            return;
+
+        min = 0f;
+        precision = 0.1f;
+    }
+}
