b58e01f873f0f9a5ebd9a84dca12cca94683ab71
chris-b-90 <kedjown@gmail.com>
2026-09-18 02:33:12 +0100

    Suspicious food
    update assembly for allowing nutritionless items to be added to food recipe
    all xml packed into the one file for relevancy/ease

---
+++ 1.6/Source/MSSFP/Recipes/IngredientValueGetter_Nutritionless.cs
@@ -0,0 +1,20 @@
+﻿using RimWorld;
+using Verse;
+
+namespace MSSFP.Recipes;
+
+public class IngredientValueGetter_Nutritionless : IngredientValueGetter
+{
+    public override float ValuePerUnitOf(ThingDef thingDef)
+    {
+        if (thingDef.IsNutritionGivingIngestible)
+            return thingDef.GetStatValueAbstract(StatDefOf.Nutrition);
+        return thingDef.IsStuff ? thingDef.VolumePerUnit : 1f;
+    }
+
+    public override string BillRequirementsDescription(RecipeDef recipe, IngredientCount ingredient)
+    {
+        string baseCount = ingredient.GetBaseCount().ToString();
+        return ingredient.IsFixedIngredient ? $"{baseCount}x {ingredient.filter.Summary}" : $"{baseCount}x {"BillNutrition".Translate()} ({ingredient.filter.Summary})";
+    }
+}
